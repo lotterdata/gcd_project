d0730770ce9b317dc535776e20f3c7b3859a5c13
lotterdata <sgpenrice@gmail.com>
2015-03-14 14:31:39 -0400

    Improve getting x data and get labels and variables

--- run_analysis.R
+++ run_analysis.R
@@ -1,34 +1,29 @@
 options(digits=8)
 
+#Load the test data
 subject.test <- readLines("test/subject_test.txt")
 
-
-x.test <- scan("test/X_test.txt",what='numeric',strip.white=TRUE)
-x.test <- matrix(as.double(x.test), nrow = 2947, ncol = 561, byrow=TRUE)
-
+x.test <- read.table("test/X_test.txt")
 
 y.test <- readLines("test/y_test.txt")
 
-
-comb.test <- data.frame(cbind(subject=subject.test,y=y.test,x.test))
+comb.test <- data.frame(cbind(subject=subject.test,label=y.test,x.test))
 rm(subject.test,x.test,y.test)
 
-
-
+#Load the training data
 subject.train <- readLines("train/subject_train.txt")
 
-
-x.train <- scan("train/X_train.txt",what='numeric',strip.white=TRUE)
-x.train <- matrix(as.double(x.train), nrow = 7352, ncol = 561, byrow=TRUE)
-
+x.train <- read.table("train/X_train.txt")
 
 y.train <- readLines("train/y_train.txt")
 
-
-comb.train <- data.frame(cbind(subject=subject.train,y=y.train,x.train))
+comb.train <- data.frame(cbind(subject=subject.train,label=y.train,x.train))
 rm(subject.train,x.train,y.train)
 
+#Combine test and training data
 comb.all <- rbind(comb.test,comb.train)
 rm(comb.test,comb.train)
 
-
+#Get labels and variable names
+feature.table <- read.table("features.txt")
+activity.table <- read.table("activity_labels.txt")
