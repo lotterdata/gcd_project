832e0a76514ac0fe7935e6c95a7097350dda340d
lotterdata <sgpenrice@gmail.com>
2015-03-15 19:09:51 -0400

    add descriptive labels

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,5 @@
 library(stringr)
+library(magrittr)
 library(plyr)
 
 #Step 1: Merge the training and the test sets to create one data set.
@@ -7,7 +8,6 @@ library(plyr)
 subject.test <- readLines("test/subject_test.txt")
 
 x.test <- read.table("test/X_test.txt")
-#names(x.test) <- feature.list$V2
 
 y.test <- readLines("test/y_test.txt")
 
@@ -18,7 +18,6 @@ rm(subject.test,x.test,y.test)
 subject.train <- readLines("train/subject_train.txt")
 
 x.train <- read.table("train/X_train.txt")
-#names(x.train) <- feature.list$V2
 
 y.train <- readLines("train/y_train.txt")
 
@@ -44,3 +43,27 @@ names(activity.table) <- c("id","activity")
 tidy.merge <- merge(tidy.select,activity.table,by.x="label",by.y="id")
 tidy.prelim <- cbind(tidy.merge[2],activity=tidy.merge[69],tidy.merge[,3:68])
 rm(activity.table,tidy.select,tidy.merge)
+
+#Step 4: Appropriately labels the data set with descriptive variable names. 
+
+#Function for calculating descriptive names
+DescriptName <- function(x){
+    domain <- ifelse(str_sub(x,start=1,end=1)=="t","Time","Frequency")
+    root <- str_sub(x,start=2,end=str_locate(x,"-")[1]-1) %>%
+        str_replace_all("Acc","Accelerometer") %>%
+        str_replace_all("Gyro","Gyroscope")
+    last.char.pos <- str_locate_all(x,"-")[[1]][2] +1
+    end <- str_sub(x,start=last.char.pos,end=last.char.pos)
+    direct <- ifelse(end %in% c("X","Y","Z"),end,"magnitude")
+    if(direct=="magnitude"){
+        root <- str_sub(root,start=1,end=str_length(root)-3)
+    }
+    type <- str_sub(x,start=str_locate(x,"-")[1]+1,end=str_locate(x,"\\(")[1]-1)
+    return(str_c(domain,".",root,".",direct,".",type))
+}
+
+#Apply this function to selected features
+select.features <- feature.list$V2[select.vars]
+descript.names <- sapply(select.features,DescriptName)
+names(tidy.prelim) <- c("Subject","Activity",descript.names)
+rm(feature.list,select.vars,select.features,DescriptName,descript.names)
