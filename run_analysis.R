38c4811363d65e250036f88c0b10792132df337a
lotterdata <sgpenrice@gmail.com>
2015-03-14 22:38:41 -0400

    write run_analysis.R through step 3

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 library(stringr)
-options(digits=8)
+library(plyr)
 
 #Step 1: Merge the training and the test sets to create one data set.
 
@@ -7,6 +7,7 @@ options(digits=8)
 subject.test <- readLines("test/subject_test.txt")
 
 x.test <- read.table("test/X_test.txt")
+#names(x.test) <- feature.list$V2
 
 y.test <- readLines("test/y_test.txt")
 
@@ -17,6 +18,7 @@ rm(subject.test,x.test,y.test)
 subject.train <- readLines("train/subject_train.txt")
 
 x.train <- read.table("train/X_train.txt")
+#names(x.train) <- feature.list$V2
 
 y.train <- readLines("train/y_train.txt")
 
@@ -25,20 +27,22 @@ rm(subject.train,x.train,y.train)
 
 #Combine test and training data
 comb.all <- rbind(comb.test,comb.train)
+
 rm(comb.test,comb.train)
-
-#Get labels and variable names
-feature.table <- read.table("features.txt")
-activity.table <- read.table("activity_labels.txt")
-
 
 
 #Step 2: Extract only the measurements on the mean and standard deviation 
 #for each measurement.
-mean.vars <- sapply(feature.table$V2,str_detect,"mean")
-std.vars <- sapply(feature.table$V2,str_detect,"std")
+feature.list <- read.table("features.txt")
+mean.vars <- sapply(feature.list$V2,str_detect,"mean")
+std.vars <- sapply(feature.list$V2,str_detect,"std")
 select.vars <- which(mean.vars|std.vars)
-tidy.prelim <- comb.all[,c(1,2,2+select.vars)]
+tidy.select<- comb.all[,c(1,2,2+select.vars)]
+rm(comb.all,mean.vars,std.vars)
 
-
-
+#Step 3: Use descriptive activity names to name the activities in the data set
+activity.table <- read.table("activity_labels.txt")
+names(activity.table) <- c("id","activity")
+tidy.merge <- merge(tidy.select,activity.table,by.x="label",by.y="id")
+tidy.prelim <- cbind(tidy.merge[2],activity=tidy.merge[82],tidy.merge[,3:81])
+rm(activity.table,tidy.select,tidy.merge)
