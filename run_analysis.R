49bd4cb301390be59f4979612d98417b47d26168
lotterdata <sgpenrice@gmail.com>
2015-03-15 12:55:19 -0400

    Correct feature selection

--- run_analysis.R
+++ run_analysis.R
@@ -27,15 +27,13 @@ rm(subject.train,x.train,y.train)
 
 #Combine test and training data
 comb.all <- rbind(comb.test,comb.train)
-
 rm(comb.test,comb.train)
-
 
 #Step 2: Extract only the measurements on the mean and standard deviation 
 #for each measurement.
 feature.list <- read.table("features.txt")
-mean.vars <- sapply(feature.list$V2,str_detect,"mean")
-std.vars <- sapply(feature.list$V2,str_detect,"std")
+mean.vars <- sapply(feature.list$V2,str_detect,"mean\\(")
+std.vars <- sapply(feature.list$V2,str_detect,"std\\(")
 select.vars <- which(mean.vars|std.vars)
 tidy.select<- comb.all[,c(1,2,2+select.vars)]
 rm(comb.all,mean.vars,std.vars)
@@ -44,5 +42,5 @@ rm(comb.all,mean.vars,std.vars)
 activity.table <- read.table("activity_labels.txt")
 names(activity.table) <- c("id","activity")
 tidy.merge <- merge(tidy.select,activity.table,by.x="label",by.y="id")
-tidy.prelim <- cbind(tidy.merge[2],activity=tidy.merge[82],tidy.merge[,3:81])
+tidy.prelim <- cbind(tidy.merge[2],activity=tidy.merge[69],tidy.merge[,3:68])
 rm(activity.table,tidy.select,tidy.merge)
