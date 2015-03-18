d0929881491f7ecc759216e734dfa2e7cb3c2cb1
lotterdata <sgpenrice@gmail.com>
2015-03-17 21:45:43 -0400

    First complete code and bare minimum for readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is a stub.
+The final product of this project can be read by placing the file "finaltidy.txt" in the working directory and running the command
+
+df <- read.table("finaltidy.txt",header=TRUE)
--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,20 @@
 library(stringr)
 library(magrittr)
-library(plyr)
+library(dplyr)
 
 #Step 1: Merge the training and the test sets to create one data set.
 
 #Load the test data
 subject.test <- readLines("test/subject_test.txt")
-
 x.test <- read.table("test/X_test.txt")
-
 y.test <- readLines("test/y_test.txt")
-
 comb.test <- data.frame(cbind(subject=subject.test,label=y.test,x.test))
 rm(subject.test,x.test,y.test)
 
 #Load the training data
 subject.train <- readLines("train/subject_train.txt")
-
 x.train <- read.table("train/X_train.txt")
-
 y.train <- readLines("train/y_train.txt")
-
 comb.train <- data.frame(cbind(subject=subject.train,label=y.train,x.train))
 rm(subject.train,x.train,y.train)
 
@@ -54,16 +48,95 @@ DescriptName <- function(x){
         str_replace_all("Gyro","Gyroscope")
     last.char.pos <- str_locate_all(x,"-")[[1]][2] +1
     end <- str_sub(x,start=last.char.pos,end=last.char.pos)
-    direct <- ifelse(end %in% c("X","Y","Z"),end,"magnitude")
-    if(direct=="magnitude"){
+    direct <- ifelse(end %in% c("X","Y","Z"),end,"Magnitude")
+    if(direct=="Magnitude"){
         root <- str_sub(root,start=1,end=str_length(root)-3)
     }
     type <- str_sub(x,start=str_locate(x,"-")[1]+1,end=str_locate(x,"\\(")[1]-1)
-    return(str_c(domain,".",root,".",direct,".",type))
+    return(str_c(type,".",root,".",direct,".",domain))
 }
 
 #Apply this function to selected features
 select.features <- feature.list$V2[select.vars]
 descript.names <- sapply(select.features,DescriptName)
 names(tidy.prelim) <- c("Subject","Activity",descript.names)
+#write.table(descript.names,"field_names.csv")
 rm(feature.list,select.vars,select.features,DescriptName,descript.names)
+
+#Step 5: From the data set in step 4, create a second, independent 
+#        tidy data set with the average of each variable for each activity 
+#        and each subject.
+
+
+tidy.final <- group_by(tidy.prelim,Subject,Activity) %>%
+    summarise(
+        mean.BodyAccelerometer.X.Time=mean(mean.BodyAccelerometer.X.Time),
+        mean.BodyAccelerometer.Y.Time=mean(mean.BodyAccelerometer.Y.Time),
+        mean.BodyAccelerometer.Z.Time=mean(mean.BodyAccelerometer.Z.Time),
+        std.BodyAccelerometer.X.Time=mean(std.BodyAccelerometer.X.Time),
+        std.BodyAccelerometer.Y.Time=mean(std.BodyAccelerometer.Y.Time),
+        std.BodyAccelerometer.Z.Time=mean(std.BodyAccelerometer.Z.Time),
+        mean.GravityAccelerometer.X.Time=mean(mean.GravityAccelerometer.X.Time),
+        mean.GravityAccelerometer.Y.Time=mean(mean.GravityAccelerometer.Y.Time),
+        mean.GravityAccelerometer.Z.Time=mean(mean.GravityAccelerometer.Z.Time),
+        std.GravityAccelerometer.X.Time=mean(std.GravityAccelerometer.X.Time),
+        std.GravityAccelerometer.Y.Time=mean(std.GravityAccelerometer.Y.Time),
+        std.GravityAccelerometer.Z.Time=mean(std.GravityAccelerometer.Z.Time),
+        mean.BodyAccelerometerJerk.X.Time=mean(mean.BodyAccelerometerJerk.X.Time),
+        mean.BodyAccelerometerJerk.Y.Time=mean(mean.BodyAccelerometerJerk.Y.Time),
+        mean.BodyAccelerometerJerk.Z.Time=mean(mean.BodyAccelerometerJerk.Z.Time),
+        std.BodyAccelerometerJerk.X.Time=mean(std.BodyAccelerometerJerk.X.Time),
+        std.BodyAccelerometerJerk.Y.Time=mean(std.BodyAccelerometerJerk.Y.Time),
+        std.BodyAccelerometerJerk.Z.Time=mean(std.BodyAccelerometerJerk.Z.Time),
+        mean.BodyGyroscope.X.Time=mean(mean.BodyGyroscope.X.Time),
+        mean.BodyGyroscope.Y.Time=mean(mean.BodyGyroscope.Y.Time),
+        mean.BodyGyroscope.Z.Time=mean(mean.BodyGyroscope.Z.Time),
+        std.BodyGyroscope.X.Time=mean(std.BodyGyroscope.X.Time),
+        std.BodyGyroscope.Y.Time=mean(std.BodyGyroscope.Y.Time),
+        std.BodyGyroscope.Z.Time=mean(std.BodyGyroscope.Z.Time),
+        mean.BodyGyroscopeJerk.X.Time=mean(mean.BodyGyroscopeJerk.X.Time),
+        mean.BodyGyroscopeJerk.Y.Time=mean(mean.BodyGyroscopeJerk.Y.Time),
+        mean.BodyGyroscopeJerk.Z.Time=mean(mean.BodyGyroscopeJerk.Z.Time),
+        std.BodyGyroscopeJerk.X.Time=mean(std.BodyGyroscopeJerk.X.Time),
+        std.BodyGyroscopeJerk.Y.Time=mean(std.BodyGyroscopeJerk.Y.Time),
+        std.BodyGyroscopeJerk.Z.Time=mean(std.BodyGyroscopeJerk.Z.Time),
+        mean.BodyAccelerometer.Magnitude.Time=mean(mean.BodyAccelerometer.Magnitude.Time),
+        std.BodyAccelerometer.Magnitude.Time=mean(std.BodyAccelerometer.Magnitude.Time),
+        mean.GravityAccelerometer.Magnitude.Time=mean(mean.GravityAccelerometer.Magnitude.Time),
+        std.GravityAccelerometer.Magnitude.Time=mean(std.GravityAccelerometer.Magnitude.Time),
+        mean.BodyAccelerometerJerk.Magnitude.Time=mean(mean.BodyAccelerometerJerk.Magnitude.Time),
+        std.BodyAccelerometerJerk.Magnitude.Time=mean(std.BodyAccelerometerJerk.Magnitude.Time),
+        mean.BodyGyroscope.Magnitude.Time=mean(mean.BodyGyroscope.Magnitude.Time),
+        std.BodyGyroscope.Magnitude.Time=mean(std.BodyGyroscope.Magnitude.Time),
+        mean.BodyGyroscopeJerk.Magnitude.Time=mean(mean.BodyGyroscopeJerk.Magnitude.Time),
+        std.BodyGyroscopeJerk.Magnitude.Time=mean(std.BodyGyroscopeJerk.Magnitude.Time),
+        mean.BodyAccelerometer.X.Frequency=mean(mean.BodyAccelerometer.X.Frequency),
+        mean.BodyAccelerometer.Y.Frequency=mean(mean.BodyAccelerometer.Y.Frequency),
+        mean.BodyAccelerometer.Z.Frequency=mean(mean.BodyAccelerometer.Z.Frequency),
+        std.BodyAccelerometer.X.Frequency=mean(std.BodyAccelerometer.X.Frequency),
+        std.BodyAccelerometer.Y.Frequency=mean(std.BodyAccelerometer.Y.Frequency),
+        std.BodyAccelerometer.Z.Frequency=mean(std.BodyAccelerometer.Z.Frequency),
+        mean.BodyAccelerometerJerk.X.Frequency=mean(mean.BodyAccelerometerJerk.X.Frequency),
+        mean.BodyAccelerometerJerk.Y.Frequency=mean(mean.BodyAccelerometerJerk.Y.Frequency),
+        mean.BodyAccelerometerJerk.Z.Frequency=mean(mean.BodyAccelerometerJerk.Z.Frequency),
+        std.BodyAccelerometerJerk.X.Frequency=mean(std.BodyAccelerometerJerk.X.Frequency),
+        std.BodyAccelerometerJerk.Y.Frequency=mean(std.BodyAccelerometerJerk.Y.Frequency),
+        std.BodyAccelerometerJerk.Z.Frequency=mean(std.BodyAccelerometerJerk.Z.Frequency),
+        mean.BodyGyroscope.X.Frequency=mean(mean.BodyGyroscope.X.Frequency),
+        mean.BodyGyroscope.Y.Frequency=mean(mean.BodyGyroscope.Y.Frequency),
+        mean.BodyGyroscope.Z.Frequency=mean(mean.BodyGyroscope.Z.Frequency),
+        std.BodyGyroscope.X.Frequency=mean(std.BodyGyroscope.X.Frequency),
+        std.BodyGyroscope.Y.Frequency=mean(std.BodyGyroscope.Y.Frequency),
+        std.BodyGyroscope.Z.Frequency=mean(std.BodyGyroscope.Z.Frequency),
+        mean.BodyAccelerometer.Magnitude.Frequency=mean(mean.BodyAccelerometer.Magnitude.Frequency),
+        std.BodyAccelerometer.Magnitude.Frequency=mean(std.BodyAccelerometer.Magnitude.Frequency),
+        mean.BodyBodyAccelerometerJerk.Magnitude.Frequency=mean(mean.BodyBodyAccelerometerJerk.Magnitude.Frequency),
+        std.BodyBodyAccelerometerJerk.Magnitude.Frequency=mean(std.BodyBodyAccelerometerJerk.Magnitude.Frequency),
+        mean.BodyBodyGyroscope.Magnitude.Frequency=mean(mean.BodyBodyGyroscope.Magnitude.Frequency),
+        std.BodyBodyGyroscope.Magnitude.Frequency=mean(std.BodyBodyGyroscope.Magnitude.Frequency),
+        mean.BodyBodyGyroscopeJerk.Magnitude.Frequency=mean(mean.BodyBodyGyroscopeJerk.Magnitude.Frequency),
+        std.BodyBodyGyroscopeJerk.Magnitude.Frequency=mean(std.BodyBodyGyroscopeJerk.Magnitude.Frequency)
+        ) 
+write.table(tidy.prelim,"prelimtidy.txt",row.name=FALSE)
+write.table(tidy.final,"finaltidy.txt",row.name=FALSE)
+rm(tidy.prelim,tidy.final)
